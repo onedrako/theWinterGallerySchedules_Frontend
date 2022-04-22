dd48c8db8b792f15e83c0e8f4677a7b7371e53ab
onedrako <onedrako@gmail.com>
2022-04-21 20:13:11 -0500

    Form for colors options working correctly using formik components

--- components/update/ListOfDays.jsx
+++ components/update/ListOfDays.jsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react'
 import moment from 'moment-timezone'
 import axios from 'axios'
+import { Formik, Field, Form } from 'formik'
 
 import DayItem from './DayItem'
 
@@ -7,6 +9,9 @@ import parseDates from '../../utils/parseDates'
 import { safeHTMLObject } from '../../utils/sanitizeObject'
 
 const ListOfDays = ({ days, notes, schedules, updateData, setUpdateData }) => {
+  const [configs, setConfigs] = useState({})
+  const [isEditingOptions, setIsEditingOptions] = useState(false)
+
   const setNewWeek = (week) => {
     const actualWeek = [...days]
     const newWeek = []
@@ -25,11 +30,17 @@ const ListOfDays = ({ days, notes, schedules, updateData, setUpdateData }) => {
 
     axios.patch(`${process.env.NEXT_PUBLIC_API_URL}/days`, safeObjet)
       .then(() => setUpdateData(!updateData))
+      .then(() => setIsEditingOptions(false))
   }
+
+  useEffect(() => {
+    axios.get(`${process.env.NEXT_PUBLIC_API_URL}/configs`)
+      .then(res => setConfigs(res.data))
+      .then(() => console.log(configs))
+  }, [])
 
   return (
     <>
-    <div>
       <div className='selectWeekContainer'>
         <p>Seleccionar Semana</p>
         <input type="week" className='weekInput'
@@ -40,67 +51,97 @@ const ListOfDays = ({ days, notes, schedules, updateData, setUpdateData }) => {
           }} />
       </div>
 
-      <div className='selectWeekContainer'>
-        <p>Color de Fondo</p>
-        <input className='colorInput' type="color" />
-        <p>Color de Títulos</p>
-        <input className='colorInput' type="color" />
-        <p>Color de Textos</p>
-        <input className='colorInput' type="color" />
+      <Formik
+        initialValues = {{
+          backgroundColor: '#ffffff',
+          mainTitlesColor: '#000000',
+          mainTextsColor: '#000000'
+        }}
+        onSubmit={ async (values) => {
+          const { backgroundColor, mainTitlesColor, mainTextsColor } = values
+          const data = {
+            backgroundColor,
+            mainTitlesColor,
+            mainTextsColor
+          }
+          await axios.patch(`${process.env.NEXT_PUBLIC_API_URL}/configs/1`, data)
+        }}
+      >
+        <Form onChange={() => setIsEditingOptions(true) } >
+          <div className='selectWeekContainer'>
+          <p>Color de Fondo</p>
+          <Field type='color' name='backgroundColor' />
+          <p>Color de Títulos</p>
+          <Field type='color' name='mainTitlesColor' />
+          <p>Color de Textos</p>
+          <Field type='color' name='mainTextsColor' />
+          { isEditingOptions && <button type='submit' className='selectWeekContainer__sendChangesButton'>Guardar cambios</button>}
+          </div>
+
+        </Form>
+      </Formik>
+
+      <div className='daysContainer'>
+        { days && (days.map(day => <DayItem
+          key={`day-${day.id}`}
+          notes={notes}
+          schedules={schedules}
+          day={day}
+          setUpdate={setUpdateData}
+          updateData={updateData} />))
+        }
       </div>
-    </div>
 
-    <div className='daysContainer'>
-      { days && (days.map(day => <DayItem
-        key={`day-${day.id}`}
-        notes={notes}
-        schedules={schedules}
-        day={day}
-        setUpdate={setUpdateData}
-        updateData={updateData} />))
-      }
-    </div>
+      <style jsx>{` 
+        .weekInput{
+          width: 185px;
+          height: 35px;
+          border-radius: 10px;
+          border: 1px solid #ccc;
+          font-size: 1.5rem;
+          font-family: 'Roboto', sans-serif;
+          padding: 15px;
+        }
 
-    <style jsx>{` 
-      .weekInput{
-        width: 185px;
-        height: 35px;
-        border-radius: 10px;
-        border: 1px solid #ccc;
-        font-size: 1.5rem;
-        font-family: 'Roboto', sans-serif;
-        padding: 15px;
-      }
+        .selectWeekContainer{
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          gap: 15px;
+          margin-bottom: 20px;
+        }
 
-      .selectWeekContainer{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        gap: 15px;
-        margin-bottom: 20px;
-      }
-      .colorInput{
-        width: 25px;
-      }
+        .selectWeekContainer__sendChangesButton{
+          font-size: 1.5rem;
+          margin-left : 25px;
+          width: 150px;
+          height: 40px; 
+          border:  none;
+          border-radius: 10px;
+          cursor: pointer;
+        }
 
-      .daysContainer{
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        padding: 25px;
-        gap: 25px;
-      }
+        .colorInput{
+          width: 25px;
+        }
 
-      p {
-        font-size: 1.5rem;
-      }
+        .daysContainer{
+          display: grid;
+          grid-template-columns: repeat(3, 1fr);
+          padding: 25px;
+          gap: 25px;
+        }
 
-      h3{
-      font-size: 2rem;
-      margin-right: 15px;
-      }
-    }`}</style>
+        p {
+          font-size: 1.5rem;
+        }
 
-    </>
+        h3{
+        font-size: 2rem;
+        margin-right: 15px;
+        }
+      }`}</style>
+  </>
   )
 }
 
